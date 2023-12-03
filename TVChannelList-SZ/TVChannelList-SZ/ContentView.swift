3f2a5b9f3b8e625acb6ce97dc61b0080f951c8cd
simon-zwicker <github@zwicker.io>
2023-12-03 12:31:41 +0100

    Folder Strucure changed
    - Put Original to original folder
    - Create new Project

---
+++ TVChannelList-SZ/TVChannelList-SZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TVChannelList-SZ
+//
+//  Created by Simon Zwicker on 03.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TVChannelList-SZ/TVChannelList-SZ/TVChannelList_SZApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVChannelList_SZApp.swift
+//  TVChannelList-SZ
+//
+//  Created by Simon Zwicker on 03.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TVChannelList_SZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
